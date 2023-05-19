24dbca7bf7f01caa49f3620eaf4acd91cc133886
gabrdiias <127976037+gabrdiias@users.noreply.github.com>
2023-05-19 11:51:13 -0300

    primeira aula de Phyton- Gabs

---
+++ print.py
@@ -0,0 +1,32 @@
+print("dodoi", "AMO", end=" ")
+
+print("coco")
+
+print("quente \n \"perguntou\" \n ")
+
+print("1 + 1 =", 1+1)
+
+print("1 x 5 =", 1*5)
+
+print("12² =", 12**2)
+
+print("30³ =", pow(30,3))
+
+nome = str("")
+
+nome = 1+1
+
+x = "A"
+print(x)
+
+x = 5.3
+print(x)
+
+x1 = "5"
+print("5.3 x 5")
+
+print(float(x) * int(x1))
+
+y = True
+print(y)
+
